62aa1651c5e1d821860cab315c3a1b84c9380484
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-05 15:12:54 +0100

    Continue on enemy class tomorrow

---
+++ Kata7/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Kata7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
+class Player
+{
+    public string Name { get; private set; }
+    public int Health { get; private set; }
+    public int Level { get; private set; }
+    public int Experience { get; private set; }
+
+    void Attack(int damage)
+    {
+        Console.WriteLine($"Player attacks the enemy dealing {damage} damage");
+    }
+
+    void GainExperience(int experience)
+    {
+        Experience += experience;
+        Console.WriteLine($"Player gained {experience} experience");
+    }
+}
+
+class Enemy // continue from here tomorrow!
+{
+    
+}
