ca00330c691397cb5c77a3f319cfdab845b798e9
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-05 13:28:01 +0100

    Initial commit

---
+++ MiniKata5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace YellowBelt;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Attack(20);
+        Heal(40);
+        
+        static void Attack(int damage)
+        {
+            Console.WriteLine($"{damage} of damage is dealt");
+        }
+
+        static void Heal(int healAmount)
+        {
+            Console.WriteLine($"{healAmount} of health is added");
+        }
+    }
+}
