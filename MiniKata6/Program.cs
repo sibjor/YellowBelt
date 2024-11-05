79a10445524a1b44bbd3b04e579a52ffdd3bbab2
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-05 13:54:28 +0100

    MiniKata6

---
+++ MiniKata6/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MiniKata6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] enemies = {"Goblin", "Orc", "Troll"};
+        var items = new List<string> {"Sword", "Shield", "Potion"};
+        
+        Console.WriteLine("Enemies:");
+        PrintEnemies();
+        Console.WriteLine("\nItems in inventory:");
+        PrintInventory();
+        items.Add("Helmet");
+        Console.WriteLine("\nUpdated inventory:");
+        PrintInventory();
+
+        void PrintEnemies()
+        {
+            foreach (string enemy in enemies)
+            {
+                Console.WriteLine(enemy);
+            }
+        }
+
+        void PrintInventory()
+        {
+            foreach (string item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
