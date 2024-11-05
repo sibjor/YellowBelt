d33f6ce9c0d8fe223f56b7485f9fa3647b2dbca3
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-05 13:32:35 +0100

    Kata5

---
+++ Kata5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Kata5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AttackEnemy("Orc", 20);
+        HealPlayer("Player", 10);
+        void AttackEnemy(string enemyName, int damage)
+        {
+            Console.WriteLine($"{damage} of damage dealt to {enemyName}");
+        }
+
+        void HealPlayer(string playerName, int healAmount)
+        {
+            Console.WriteLine($"{playerName} heals with {healAmount} points");
+        }
+    }
+}
