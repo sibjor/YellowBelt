81aa59ff1d3e941c1aeb1b9fca42a687b3caf420
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-06 09:44:07 +0100

    Kata7

--- Kata7/Program.cs
+++ Kata7/Program.cs
@@ -4,7 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        int playerDamage = 15; // not supposed to be included in the class as previously defined on the Github page
+        int xpPostVictory = 10;
         
+        var player = new Player("Player", 25, 5, 50);
+        Enemy enemy = new("Orc", 75, 10);
+        
+        player.Attack(playerDamage);
+        enemy.TakeDamage(playerDamage);
+        
+        player.GainExperience(xpPostVictory);
     }
 }
 
@@ -15,19 +24,46 @@ class Player
     public int Level { get; private set; }
     public int Experience { get; private set; }
 
-    void Attack(int damage)
+    public Player(string name, int health, int level, int experience)
+    {
+        Name = name;
+        Health = health;
+        Level = level;
+        Experience = experience;
+    }
+
+    public void Attack(int damage)
     {
         Console.WriteLine($"Player attacks the enemy dealing {damage} damage");
     }
 
-    void GainExperience(int experience)
+    public void GainExperience(int experience)
     {
         Experience += experience;
         Console.WriteLine($"Player gained {experience} experience");
     }
 }
 
-class Enemy // continue from here tomorrow!
+class Enemy 
 {
-    
+    public string Type { get; private set; }
+    public int Health { get; private set; }
+    public int Damage { get; private set; }
+
+    public Enemy(string type, int health, int damage)
+    {
+        Type = type;
+        Health = health;
+        Damage = damage;
+    }
+
+    public void TakeDamage(int damage) // not the same damage as above
+    {
+        Health -= damage;
+        Console.WriteLine($"{Type} takes {damage} damage");
+        if (Health <= 0)
+        {
+            Console.WriteLine("Enemy defeated");
+        }
+    }
 }
